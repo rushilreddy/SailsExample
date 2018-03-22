1f82e8493d0a6d1ed7780212ee3723d3cdd5349c
Roosh <rushilvreddy@gmail.com>
2018-03-22 14:20:05 -0700

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# FirstSail
+
+a [Sails](http://sailsjs.org) application
---
+++ assets/js/spa.js
@@ -0,0 +1,26 @@
+angular.module('Platzi', []);
+angular.module('Platzi').controller('BaseCtrl', ['$scope', '$http', function ($scope, $http){
+
+    //instead of $http.get use io.socket for realtime
+    io.socket.get('/emoji', function(data){
+        $scope.emojis = data;
+        //angular auto (digest loop) handles this w/ http. this is the way to tap in and have it autorender
+        $scope.$apply();
+    });
+
+    io.socket.on('/emoji', function(evemt){
+        switch(event.verb) {
+            case 'created':
+                $scope.emojis.push(event.data);
+                $scope.$apply();
+                break;
+        }
+    });
+
+}]);
+
+/*
+
+asdasdas
+
+*/
